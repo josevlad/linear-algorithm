fc45ee111e0b479c085d204d5205955ed300f15f
Vladimir Castañeda <vladimir29castaneda@gmail.com>
2020-07-22 21:35:47 -0300

    exercise-one

---
+++ exercise-one/src/MyApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("Programa que suma 2 numeros dados");
+        // defino las variables
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        // habilito el ingreso de datos por el teclado
+        Scanner keyboard = new Scanner(System.in);
+
+        // leo los datos del usuario
+        System.out.print("Ingrese el primer numero para el calculo: ");
+        firstNumber = keyboard.nextInt();
+
+        System.out.print("Ingrese el segundo numero para el calculo: ");
+        secondNumber = keyboard.nextInt();
+
+        // realizo el proceso y lo guardo en la variable result
+        result = firstNumber + secondNumber;
+
+        // muestro el resultado por pantalla
+        System.out.println("El resultado es: " + result);
+    }
+}
